f133d7a1635099b9ec4befa29e9dce46bf1696bf
Ivan Zaruba <ivan.zaruba@nixsolutions.com>
2017-11-30 14:23:16 +0200

    Added source code to reproduce the issue

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+
+namespace PomeloEfCoreBug
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly string connectionString;
+
+        public ApplicationDbContext(string userName, string password)
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                Port = 3306,
+                UserID = userName,
+                Password = password,
+                Database = Program.Database
+            };
+
+            this.connectionString = builder.ConnectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseMySql(this.connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .ToTable("users");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace PomeloEfCoreBug
+{
+    internal class Program
+    {
+        internal const string Database = "pomelo_test";
+
+        private static void Main()
+        {
+            // (!) Ensure DB `pomelo_test` is deleted before running the test (!)
+
+            //ThisWorks();
+
+            RunBug();
+        }
+
+        private static void ThisWorks()
+        {
+           // (!) NOTE(!) the first connection is valid
+
+            Run("root", "@r00t@");              // DB CREATED 
+            Run(string.Empty, string.Empty);    // Access denied for user ''@'localhost' (using password: NO)
+            Run("root", "@r00t@");              // DB EXISTS
+            Run(string.Empty, string.Empty);    // Access denied for user ''@'localhost' (using password: NO)
+            Run("root", "@r00t@");              // DB EXISTS
+        }
+
+        private static void RunBug()
+        {
+            // (!) NOTE (!) the first connection is invalid
+
+            Run(string.Empty, string.Empty);    // Access denied for user ''@'localhost' (using password: NO)
+            Run("root", "@r00t@");              // Access denied for user ''@'localhost' (using password: NO) - BUG
+            Run(string.Empty, string.Empty);    // Access denied for user ''@'localhost' (using password: NO)
+            Run("root", "@r00t@");              // Access denied for user ''@'localhost' (using password: NO) - BUG
+        }
+
+        private static void Run(string name, string password)
+        {
+            var ctx = new ApplicationDbContext(name, password);
+
+            try
+            {
+                var created = ctx.Database.EnsureCreated();
+
+                Console.WriteLine(created ? "DB CREATED" : "DB EXISTS");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
